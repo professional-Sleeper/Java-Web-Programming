384969491c98a5cb279d47b45d82fb7d04437280
professional-Sleeper <44637739+professional-Sleeper@users.noreply.github.com>
2018-11-01 09:17:06 +0900

    UI & FileStream ver 1.0
    upload & branch test

---
+++ [JAVA]/UI & FileStream/src/com/scsa/ui/UITest.java
@@ -0,0 +1,12 @@
+package com.scsa.ui;
+
+public class UITest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaUI ju = new JavaUI();
+		ju.createUI();
+		ju.addEvent();
+
+	}
+}
